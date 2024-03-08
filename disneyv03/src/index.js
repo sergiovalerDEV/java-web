2e4fff2696ef5f2e4690784befcc020128ebfeaa
Alberto Hernández Akkari <akkarihdez@gmail.com>
2024-03-08 11:35:05 +0100

    [Disney] PARAMS QUERIES BODY 08-03-24

---
+++ disneyv03/src/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express(); // PATRÓN BUILDER EN TODA REGLA
+const PORT = 3000;
+
+app.use(express.json());
+
+app.listen(PORT, ()=>{
+    console.log("Servidor funcionandoooo!!!!");
+});
+/////////// QUERY PARAMSSS!!!!!!!
+// http://localhost:3000/usuarios?email=a@s.com&pass=123
+app.post("/usuarios", (req,res)=>{
+    const email = req.query.email;
+    const pass = req.query.pass;
+    // SELECT * FROM USUARIO WHERE EMAIL=' + email + 'AND pass = ' + pass + '';
+    res.send('Tu primer GET');
+    // LEER UNA PELÍCULA (SELECT * FROM PELICULAS)
+});
+/////////// PATH PARAMSSS!!!!!!!
+app.get("/usuarios/:nombre", (req,res)=>{
+    const nombre = req.params.nombre;
+    res.send('Params por GET');
+});
+/////////// BODY PARAMSSS!!!!!!!
+/* http://localhost:3000/usuarios
+    {
+    "email" : "a@svalero.com",
+    "password": "1234",
+    "name": "albert"
+    }
+*/
+app.post("/usuarios/", (req,res)=>{
+    const {email, password, name} = req.body;
+    res.send('Body paramss por POST');
+});
+/*app.post("/usuarios/insert", (req,res)=>{
+    const {email, password, name} = req.body;
+    res.send('Body paramss por POST');
+});*/
+app.get("/peliculas", (req,res)=>{
+    res.send('Tu primer GET');
+    // LEER UNA PELÍCULA (SELECT * FROM PELICULAS)
+});
+app.post("/peliculas", (req,res)=>{
+    res.send('Tu primer POST');
+    // INSERTAR UNA PELÍCULA (INSERT INTO PELICULA ... VALUES ...)
+});
+app.put("/peliculas", (req,res)=>{
+    res.send('Tu primer PUT');
+    // MODIFICAR UNA PELÍCULA (UPDATE PELICULA SET .... WHERE)
+});
+app.delete("/peliculas", (req,res)=>{
+    res.send('Tu primer DELETE');
+    // ELIMINAR UNA PELÍCULA (DELETE PELICULA)
+});
+
+
